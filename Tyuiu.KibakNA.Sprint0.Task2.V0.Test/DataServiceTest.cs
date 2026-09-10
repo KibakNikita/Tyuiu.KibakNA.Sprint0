fe67d7142f4984dc1c771d893b27b6b3c4c250cd
KibakNikita <kibak14@mail.ru>
2026-09-10 14:28:28 +0300

    Task2

---
+++ Tyuiu.KibakNA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KibakNA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KibakNA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Никита";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Привет, Никита", res);
+        }
+    }
+}
